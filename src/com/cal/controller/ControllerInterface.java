474731514af02b7acdc83e6f81228889211484d2
Ouss4 <guettouche_4@hotmail.fr>
2016-10-02 16:19:58 +0200

    Create ControllerInterface.java

---
+++ src/com/cal/controller/ControllerInterface.java
@@ -0,0 +1,10 @@
+package com.cal.controller;
+
+public interface ControllerInterface {
+	
+	public void numberClicked(String str);
+	
+	public void operatorClicked(String str);
+	
+	public void reset();
+}
